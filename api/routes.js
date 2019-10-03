b78cb91a3942e0c83c6485ea79786f0939fc1e58
plusbpm <plus-bpm@yandex.ru>
2019-10-03 23:22:22 +0300

    mock autocomplete route

--- api/routes.js
+++ api/routes.js
@@ -1,7 +1,28 @@
+const get = require('lodash/get');
+
 module.exports = (fastify, { mongodb }, done) => {
   fastify.get('/test', async () => {
     await mongodb.createCollection('test2');
     return { uha: Math.random() };
+  });
+
+  fastify.get('/autocomplete', async request => {
+    const list = [
+      { id: '1', name: 'asdfgh' },
+      { id: '2', name: 'asdzxc' },
+      { id: '3', name: 'asdqwe' },
+      { id: '4', name: 'qwerty' },
+      { id: '5', name: 'qweasd' },
+      { id: '6', name: 'qwezxc' },
+      { id: '7', name: 'fghqwe' },
+      { id: '8', name: 'fghvbn' },
+      { id: '9', name: 'fghzxc' },
+      { id: '10', name: 'rtyfgh' },
+    ];
+    const q = get(request, 'query.q');
+    const regex = new RegExp(q, 'gi');
+
+    return list.filter(({ name }) => regex.test(name));
   });
 
   fastify.all('/*', async (request, reply) => {
