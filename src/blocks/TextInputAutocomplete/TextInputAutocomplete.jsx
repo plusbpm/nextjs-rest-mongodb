7479effe9607e7f256a93b253c5aa1aa3aed47a9
plusbpm <plus-bpm@yandex.ru>
2019-10-03 23:22:02 +0300

    autocomplete field

---
+++ src/blocks/TextInputAutocomplete/TextInputAutocomplete.jsx
@@ -0,0 +1,216 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import throttle from 'lodash/throttle';
+import isEqual from 'lodash/isEqual';
+
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+import { withInquery } from '../../restClient';
+import TextInput from '../TextInput';
+
+class TextInputAutocomplete extends Component {
+  constructor(props) {
+    super(props);
+
+    this.throttledHandler = throttle(this.handleValue, 300, { leading: false });
+    this.state = {
+      highlighted: null,
+      lastValue: props.defaultValue,
+      reseted: false,
+      visible: false,
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeydown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeydown);
+  }
+
+  get inquery() {
+    return this.props.autocompleteInquery;
+  }
+
+  get inqueryState() {
+    const inqueryState = this.inquery.getState();
+    return {
+      ...inqueryState,
+      list: inqueryState.data || [],
+    };
+  }
+
+  get listContent() {
+    const { list, isLoading } = this.inqueryState;
+    const { highlighted, visible } = this.state;
+
+    if (!visible) return null;
+
+    if (list.length === 0 && isLoading)
+      return (
+        <ListItem dense>
+          <CircularProgress size={16} style={{ marginRight: 10 }} />
+          <ListItemText primary={<span>Loading...</span>} />
+        </ListItem>
+      );
+
+    if (list.length === 0 && !isLoading)
+      return (
+        <ListItem dense>
+          <ListItemText primary="Nothing" />
+        </ListItem>
+      );
+
+    if (list.length > 0)
+      return list.map(({ id, name }, index) => (
+        <ListItem
+          key={id}
+          dense
+          disabled={isLoading}
+          button
+          selected={highlighted === index}
+          onMouseDown={() => this.select(index)}
+        >
+          <ListItemText primary={name} />
+        </ListItem>
+      ));
+
+    return null;
+  }
+
+  handleInputRef = ref => {
+    const { handleInputRef } = this.props;
+    this.inputRef = ref;
+    if (handleInputRef) handleInputRef(ref);
+  };
+
+  handleFocus = () => {
+    const { lastValue } = this.state;
+    const { data } = this.inqueryState;
+    if (!data && lastValue.length >= this.props.minLength) {
+      this.inquery.send({ query: { q: lastValue } });
+    }
+    this.patchState({ visible: true });
+  };
+
+  handleChange = event => {
+    const trimmedValue = (event.target.value || '').trim();
+    const { lastValue, reseted } = this.state;
+    const { onReset, minLength } = this.props;
+
+    if (trimmedValue === lastValue) return;
+    if (!reseted) onReset();
+
+    const patch = { visible: true, reseted: true, lastValue: trimmedValue };
+    if (trimmedValue.length >= minLength) {
+      this.throttledHandler(trimmedValue);
+    }
+    this.patchState(patch);
+  };
+
+  handleValue = value => {
+    this.patchState({ highlighted: null });
+    this.inquery.send({ query: { q: value } });
+  };
+
+  handleKeydown = event => {
+    const { highlighted, visible } = this.state;
+    const { list, isLoading } = this.inqueryState;
+
+    if (!visible || isLoading) return;
+    const noHighlighted = highlighted === null;
+
+    const patch = {};
+    switch (event.keyCode) {
+      case 38: {
+        // up
+        event.preventDefault();
+        const nextHighlighted = noHighlighted ? list.length - 1 : highlighted - 1;
+        patch.highlighted = Math.max(0, nextHighlighted);
+        break;
+      }
+      case 40: {
+        // down
+        event.preventDefault();
+        const nextHighlighted = noHighlighted ? 0 : highlighted + 1;
+        patch.highlighted = Math.min(list.length - 1, nextHighlighted);
+        break;
+      }
+      case 27: {
+        // esc
+        event.preventDefault();
+        this.inputRef.blur();
+        break;
+      }
+      case 13: {
+        // enter
+        event.preventDefault();
+        this.select(highlighted);
+        break;
+      }
+      default:
+    }
+    this.patchState(patch);
+  };
+
+  patchState = patch => {
+    const nextState = { ...this.state, ...patch };
+    if (isEqual(this.state, nextState)) return;
+    this.setState(nextState);
+  };
+
+  select = index => {
+    const item = this.inqueryState.list[index];
+    this.patchState({ reseted: false, visible: false });
+    this.props.onSelect(item);
+  };
+
+  render() {
+    const {
+      autocompleteInquery,
+      endpoint,
+      handleInputRef,
+      minLength,
+      onReset,
+      onSelect,
+      ...rest
+    } = this.props;
+    return (
+      <>
+        <TextInput
+          {...rest}
+          onChange={this.handleChange}
+          onFocus={this.handleFocus}
+          onBlur={() => this.patchState({ visible: false })}
+          autoComplete="off"
+          inputRef={this.handleInputRef}
+        />
+        {this.state.visible && <List>{this.listContent}</List>}
+      </>
+    );
+  }
+}
+
+TextInputAutocomplete.propTypes = {
+  autocompleteInquery: PropTypes.shape().isRequired,
+  defaultValue: PropTypes.string,
+  endpoint: PropTypes.string.isRequired,
+  handleInputRef: PropTypes.func,
+  minLength: PropTypes.number,
+  onReset: PropTypes.func.isRequired,
+  onSelect: PropTypes.func.isRequired,
+};
+
+TextInputAutocomplete.defaultProps = {
+  handleInputRef: undefined,
+  defaultValue: '',
+  minLength: 2,
+};
+
+const propsToOptions = ({ endpoint }) => ({ endpoint });
+
+export default withInquery('autocomplete', propsToOptions)(TextInputAutocomplete);
