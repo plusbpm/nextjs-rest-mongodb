3e7fd8af6a8b8d2b2d1ef6caa277f1682a5e368a
plusbpm <plus-bpm@yandex.ru>
2019-10-03 23:22:32 +0300

    transaction form

--- src/blocks/TransactionForm/TransactionForm.jsx
+++ src/blocks/TransactionForm/TransactionForm.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
 import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
+import useForm from 'react-hook-form';
 
 import BackWithFallback from '../BackWithFallback';
+import ButtonWithSpinner from '../ButtonWithSpinner';
 import TextInput from '../TextInput';
+import TextInputAutocomplete from '../TextInputAutocomplete';
 import { LAYOUT_BREAKPOINT } from '../constants';
+import { useInquery } from '../../restClient';
 
 const useStyles = makeStyles(theme => ({
   actions: {
@@ -21,24 +24,81 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
+const makeSubmitHandler = (transactionInquery, handleSubmit) =>
+  handleSubmit(({ amount, correspondentID: id }, event) => {
+    event.preventDefault();
+    transactionInquery.send({
+      endpoint: `/api/transaction`,
+      method: 'POST',
+      body: JSON.stringify({ amount, id }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  });
+
 function TransactionForm() {
   const { actions } = useStyles();
+  const validations = useForm();
+  const transactionInquery = useInquery('transaction');
+
+  const { errors, handleSubmit, register, setValue, triggerValidation } = validations;
+  const { correspondent, correspondentID, amount } = errors;
+  const corrError = correspondent || correspondentID;
+
   return (
-    <form>
-      <TextInput name="correspondent" label="Correspondent" defaultValue="" />
-      <TextField name="correspondentID" type="hidden" value="" />
+    <form onSubmit={makeSubmitHandler(transactionInquery, handleSubmit)}>
+      <TextInputAutocomplete
+        name="correspondent"
+        label="Correspondent"
+        endpoint="/autocomplete"
+        onSelect={({ id, name }) => {
+          setValue('correspondentID', id);
+          setValue('correspondent', name);
+          triggerValidation({ name: 'correspondentID' });
+        }}
+        onReset={() => {
+          setValue('correspondentID', '');
+          triggerValidation({ name: 'correspondentID' });
+        }}
+        handleInputRef={register({
+          minLength: {
+            value: 2,
+            message: 'Minimum 2 sybmols for search',
+          },
+        })}
+        error={!!corrError}
+        helperText={corrError && corrError.message}
+      />
+      <TextField
+        style={{ display: 'none' }}
+        name="correspondentID"
+        type="text"
+        inputRef={register({
+          required: 'Select from the dropdown list',
+        })}
+      />
       <TextInput
         name="amount"
         type="number"
         label="Amount"
         defaultValue=""
         inputProps={{ step: 0.01 }}
+        inputRef={register({
+          required: 'Required field',
+          max: {
+            value: 500,
+            message: 'The balance is not enough',
+          },
+        })}
+        error={!!amount}
+        helperText={amount && amount.message}
       />
       <Grid container justify="flex-end" className={actions}>
         <BackWithFallback fallback="/cabinet">Cancel</BackWithFallback>
-        <Button type="submit" color="primary" variant="contained">
+        <ButtonWithSpinner type="submit" color="primary" variant="contained">
           Submit
-        </Button>
+        </ButtonWithSpinner>
       </Grid>
     </form>
   );
