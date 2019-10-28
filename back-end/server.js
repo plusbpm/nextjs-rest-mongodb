1b578cb2f2607fc074805b66de4ee4d5e0e3be11
plusbpm <plus-bpm@yandex.ru>
2019-10-28 23:08:33 +0300

    use proxy from backend

--- back-end/server.js
+++ back-end/server.js
@@ -1,4 +1,5 @@
 require('../config/dotenv');
+const fastifyProxy = require('fastify-http-proxy');
 const initServer = require('../shared/util/initServer');
 
 const mountApi = require('./api');
@@ -6,6 +7,7 @@ const { mongodb } = require('./db');
 const createValidation = require('../shared/validation');
 
 const mockingEnabled = process.env.CREATE_MOCK_DATA === 'true';
+const upstreamHost = process.env.BACKEND_UPSTREAM || '';
 
 async function setup(server) {
   const validation = createValidation();
@@ -14,6 +16,11 @@ async function setup(server) {
   if (mockingEnabled) await dbAdapter.mocking();
 
   await mountApi(server, { dbAdapter, validation });
+
+  if (upstreamHost !== '')
+    server.register(fastifyProxy, {
+      upstream: upstreamHost,
+    });
 }
 
 initServer(setup, 'PORT_BACKEND');
--- config/webpack.js
+++ config/webpack.js
@@ -4,13 +4,19 @@ const { EnvironmentPlugin } = require('webpack');
 const merge = require('webpack-merge');
 const pick = require('lodash/pick');
 
-const pickKeys = ['API_ROOT', 'API_DOMAIN'];
+const pickKeys = ['API_ROOT'];
 const publicKeys = pick(process.env, pickKeys);
+const { API_DOMAIN_CLIENT, API_DOMAIN_SERVER } = process.env;
 
-module.exports = config =>
+module.exports = (config, { isServer }) =>
   merge(
     {
-      plugins: [new EnvironmentPlugin(publicKeys)],
+      plugins: [
+        new EnvironmentPlugin({
+          ...publicKeys,
+          API_DOMAIN: isServer ? API_DOMAIN_SERVER : API_DOMAIN_CLIENT,
+        }),
+      ],
     },
     config,
   );
